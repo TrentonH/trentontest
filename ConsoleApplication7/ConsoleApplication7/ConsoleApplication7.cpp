678a930315c349461f3e06b2718e030ed6b9db86
TrentonH <trentonwhamilton@gmail.com>
2014-12-29 12:19:34 -0700

    test
    random infinite loop 1 or 0

---
+++ ConsoleApplication7/ConsoleApplication7/ConsoleApplication7.cpp
@@ -0,0 +1,24 @@
+// ConsoleApplication7.cpp : Defines the entry point for the console application.
+//
+#include <iostream>
+#include "stdafx.h"
+#include <ctime>
+#include <cstdlib>
+using namespace std;
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+
+	for (int i = 0; i >= 0; i++)
+	{
+		for (int j = 0; j < 5000000; ++j)
+		{
+		}
+		int random = (rand() % 2);
+			cout << random;
+
+	}
+	return 0;
+}
+
